76a14f94488271d17049080db049e96e60504700
yashwant dangi <dangi.yd100@gmail.com>
2022-01-10 01:42:25 +0530

    client - post create

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Postcreate from "./PostCreate";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <Postcreate />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/PostCreate.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Postcreate = () => {
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <label>Title</label>
+          <input className="form-control"></input>
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Postcreate;
