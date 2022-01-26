9f95d7ec16d74013d0e2c768ecd37d9ec55cb1a0
yashwant dangi <dangi.yd100@gmail.com>
2022-01-27 02:10:43 +0530

    postlist component done

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -13,8 +13,26 @@ function PostList() {
     fetchPosts();
   }, []);
 
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
   console.log(posts);
-  return <div>PostList</div>;
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
 }
 
 export default PostList;
