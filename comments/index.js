908eeddea80585f4f5a296322150c2e092f8bd5e
yashwant dangi <dangi.yd100@gmail.com>
2022-03-10 21:16:04 +0530

    moderation service added

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
   await axios.post("http://localhost:4005/events", {
@@ -28,6 +28,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
   res.status(201).send(comments);
--- run.md
+++ run.md
@@ -1,1 +1,3 @@
-open -a iTerm Documents/blog/client Documents/blog/comments Documents/blog/posts Documents/blog/event-bus Documents/blog/query
+open -a iTerm Documents/blog/client Documents/blog/comments Documents/blog/posts Documents/blog/event-bus Documents/blog/query Documents/blog/moderation
+
+npm run start
