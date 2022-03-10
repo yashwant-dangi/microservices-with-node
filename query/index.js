d8c468753389db403aec0facb0cce23037fa7358
yashwant dangi <dangi.yd100@gmail.com>
2022-03-10 22:00:06 +0530

    comment and query service updated to use moderation service

--- query/index.js
+++ query/index.js
@@ -15,17 +15,27 @@ app.get("/posts", (req, res) => {
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
-  if (type == "PostCreated") {
+  if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type == "CommentCreated") {
+  if (type === "CommentCreated") {
     const { id, content, postId, status } = data;
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
 
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    comment.content = content;
+  }
   res.send({});
 });
 
