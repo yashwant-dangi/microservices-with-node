70a5bb50ff6e9d3d8bfe0a929c43ebf1d5f9a638
yashwant dangi <dangi.yd100@gmail.com>
2022-04-07 22:29:23 +0530

    common error class created

--- ticketing/auth/src/errors/database-connection-error.ts
+++ ticketing/auth/src/errors/database-connection-error.ts
@@ -1,8 +1,10 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
   reason = "Error Connecting to database";
   constructor() {
-    super();
+    super("Error connecting to db");
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
