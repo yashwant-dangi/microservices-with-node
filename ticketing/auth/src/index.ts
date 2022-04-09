c22b0505acc206676fff1ee6eb5208e08c1d2e0c
yashwant dangi <dangi.yd100@gmail.com>
2022-04-09 23:52:26 +0530

    user model created

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -17,12 +18,23 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", async () => {
-  throw new NotFoundError();
-});
+// app.all("*", async () => {
+//   throw new NotFoundError();
+// });
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {});
+    console.log("Connected to MongoDb");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!");
+  });
+};
+
+start();
