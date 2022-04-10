c00ded38d3f3e8f4e8db3246bddc5ab99b52e32b
yashwant dangi <dangi.yd100@gmail.com>
2022-04-11 00:42:16 +0530

    type checking for user model added

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
+// An interface that describes the properties
+// that are required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -10,7 +23,10 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model<any, UserModel>("User", userSchema);
 
 export { User };
