b3eeadc6ab5d2525388eced33ed17da502e71cb4
yashwant dangi <dangi.yd100@gmail.com>
2022-04-14 23:52:19 +0530

    current user and signout routes added

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,17 @@
 import express from "express";
 
+import { currentUser } from "../middlewares/current-user";
+
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+  // try {
+  //   const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+  //   res.send({ currentUser: payload });
+  // } catch (error) {
+  //   res.send({ currentUser: null });
+  // }
 });
 
 export { router as currentUserRouter };
