499159545c71e611f92658f100871abde9820a7f
yashwant dangi <dangi.yd100@gmail.com>
2022-05-05 17:24:51 +0530

    signin page added

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import buildClient from "../api/build-client";
 
-const LandingPage = (props) => {
-  console.log(props);
-  return <h1>Landing Page</h1>;
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
 export async function getServerSideProps(context) {
   const client = buildClient(context);
   const { data } = await client.get("/api/users/currentuser");
 
-  return data;
+  return {
+    props: data,
+  };
 }
 
 export default LandingPage;
