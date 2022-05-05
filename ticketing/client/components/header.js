5ea57aa5b858a3aea34daa04482fd5401a4412a9
yashwant dangi <dangi.yd100@gmail.com>
2022-05-05 20:43:48 +0530

    signout page added

--- ticketing/client/components/header.js
+++ ticketing/client/components/header.js
@@ -1,6 +1,22 @@
 import Link from "next/link";
 
 function Header({ currentUser }) {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>
+            <a className="nav-link">{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href={"/"}>
@@ -8,7 +24,8 @@ function Header({ currentUser }) {
       </Link>
       <div className="d-flex justify-content-end">
         <ul className="nav d-flex align-items-center">
-          {currentUser ? "Sign Out" : "Sign in/up"}
+          {/* {currentUser ? "Sign Out" : "Sign in/up"} */}
+          {links}
         </ul>
       </div>
     </nav>
