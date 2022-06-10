e3431e1923b4aec967462149fd71e2c798b8c7aa
yashwant dangi <dangi.yd100@gmail.com>
2022-06-10 17:10:24 +0530

    nats publisher for ticket created event

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -10,6 +10,14 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
   try {
+    await natsWrapper.connect("ticketing", "dsfsdsfs", "http://nats-srv:4222");
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to MongoDb");
   } catch (error) {
