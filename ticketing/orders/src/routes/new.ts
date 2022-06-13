d7deb3024693ad0af1b083022f116a1a3aebc01f
yashwant dangi <dangi.yd100@gmail.com>
2022-06-13 17:48:17 +0530

    new order controller added

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import { requireAuth, validateReqeust } from "@ydangi/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.get("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateReqeust,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
