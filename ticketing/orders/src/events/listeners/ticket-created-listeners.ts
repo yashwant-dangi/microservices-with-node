e85bffca86e5548ed7b1ab6bd57f921190cf0a4d
yashwant dangi <dangi.yd100@gmail.com>
2022-06-28 00:40:55 +0530

    ticket created listener implementation

---
+++ ticketing/orders/src/events/listeners/ticket-created-listeners.ts
@@ -0,0 +1,21 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketCreatedEvent } from "@ydangi/common";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { id, title, price } = data;
+    const ticket = Ticket.build({
+      title,
+      price,
+      id,
+    });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
