625cab1ef1d8572ec8d059a168dd2e0257c0fd35
yashwant dangi <dangi.yd100@gmail.com>
2022-07-13 00:07:40 +0530

    order created listener for expiration service

---
+++ ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,15 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@ydangi/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
