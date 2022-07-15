45193baa65f94ce7eeed5bc0f474a6eb2d7191aa
yashwant dangi <dangi.yd100@gmail.com>
2022-07-15 23:16:15 +0530

    created job using bull

--- ticketing/expiration/src/events/listeners/order-created-listener.ts
+++ ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -6,10 +6,17 @@ import {
 } from "@ydangi/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
 }
