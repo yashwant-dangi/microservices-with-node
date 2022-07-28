f7aa5cc2256d6a949f1f970a10bada0d8adb90fa
yashwant dangi <dangi.yd100@gmail.com>
2022-07-29 01:43:14 +0530

    tying order and charge together

--- ticketing/payments/src/routes/__test__/new.test.ts
+++ ticketing/payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import { OrderStatus } from "@ydangi/common";
 import { app } from "../../app";
 import { Order } from "../../models/order";
 import { stripe } from "../../stripe";
+import { Payment } from "../../models/payment";
 
 jest.mock("../../stripe");
 
@@ -83,4 +84,10 @@ it("returns a 204 with valid inputs", async () => {
   expect(chargeOptions.source).toEqual("tok_visa");
   expect(chargeOptions.amount).toEqual(20 * 100);
   expect(chargeOptions.currency).toEqual("usd");
+
+  const payment = Payment.findOne({
+    orderId: order.id,
+    stripeId: chargeOptions.id,
+  });
+  expect(payment).not.toBeNull();
 });
