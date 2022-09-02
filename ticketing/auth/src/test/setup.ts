7e60c8d24f4ead7ee145c75076e39cd7e827700e
yashwant dangi <dangi.yd100@gmail.com>
2022-09-03 01:02:17 +0530

    current user test

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,9 +1,13 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
 
-let mongo: any;
+declare global {
+  var signin: (id?: string) => Promise<string[]>;
+}
 
+let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "asdfasdf";
 
@@ -25,3 +29,20 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
